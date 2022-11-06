536cf5d9bee127cbe9ba3bd600cb9bf2b0a699ec
Eromtap <66063750+Eromtap@users.noreply.github.com>
2022-11-06 14:50:10 -0500

    Update sillyList.py

--- LinkedLists/sillyList.py
+++ LinkedLists/sillyList.py
@@ -1,7 +1,8 @@
+
 '''
-OK, so here's silly thing I made for no reason other than pure curiosity. Could a 
+OK, so here's silly thing I made for no reason other than sheer curiosity. Could a 
 python dict be used as a very rudimentary linked list like creature. Answer: yes it can.
-Of course it's completely useless tbh. Since a dict is a collection of key/value pairs,
+Course it's completely useless tbh. Since a dict is a collection of key/value pairs,
 I decided to make the value of each entry be the key of the next entry. Basically 
 forming a totally pointless (pun intended lol) singly linked list. Since we aren't
 storing anything but simple values, this script serves no purpose other than 
@@ -26,6 +27,7 @@ class LinkedList:
     def __init__(self, dictionary):
         self.dictionary = dictionary
         
+        
     ## Add entry to end of list
     ## Removes end of list "None" value from current last entry and 
     ##      replaces with value entered. Then adds a key/value pair with new
@@ -43,7 +45,8 @@ class LinkedList:
             if v == None:
                 self.dictionary[k] = value
         self.dictionary[value] = None
-       
+        
+        
     ## If value is a repeat, recursively run until we find a counter that is
     ##  not a repeat to append to the value
     def repeats(self,value,altered_value,counter=1):
@@ -51,7 +54,9 @@ class LinkedList:
         if altered_value in self.dictionary:
             altered_value = self.repeats(value,altered_value,counter+1)
         return altered_value
-         
+        
+    
+    
     ## Delete an entry. This is a bit different than an actual linked list
     ##      because since we arent using pointers, we cant just change pointer 
     ##      values. We also have to delete the entry itself. But pretty close.
@@ -60,18 +65,24 @@ class LinkedList:
     ## Then we just delete the target entry.
     def delete_entry(self, value):
         if value in self.dictionary:
-            next_entry = self.dictionary[value]
-            
-            for k, v in self.dictionary.items():
-                if v == value:
-                    last_entry = k
-                    
-            self.dictionary[last_entry] = next_entry                    
-            del self.dictionary[value]
+            if len(self.dictionary) <= 1:
+                self.dictionary = {}
+            else:
+                next_entry = self.dictionary[value]
+                
+                for k, v in self.dictionary.items():
+                    if v == value:
+                        last_entry = k
+                        
+                self.dictionary[last_entry] = next_entry                    
+                del self.dictionary[value]
         else:
             print(f'{value} not in list.')
-       
-      
+        
+        
+        
+        
+        
 def main():
     print('\nWelcome to a silly "linked list" script that has no utilitarian purpose, but is undoubtedly fun.')        
      
@@ -87,8 +98,8 @@ def main():
             Llist.add_entry(input_value)
             
         elif user_input == 'R' or user_input == 'r':
-            input_value == input('Enter a value to remove:  ') 
-            Llist.delete_entry(user_input)
+            input_value = input('Enter a value to remove:  ') 
+            Llist.delete_entry(input_value)
             
         elif user_input == 'V' or user_input == 'v':
             view_choice = input('(W)hole list or (S)ingle entry: ')
@@ -104,6 +115,7 @@ def main():
                     else:
                         print(f'\n{view_entry} not in list. ')
                         break
+                
                     view_next = input('(N)ext entry or (B)ack to main menu:  ')
                     if view_next == 'N' or view_next == 'n':
                         view_entry = Llist.dictionary[view_entry]
@@ -112,23 +124,32 @@ def main():
                             break
                     elif view_next == 'B' or view_next == 'b':
                         break
+                    
                     else:
                         print('\nCommand not recognized.')
                         break
+            
+            
             else:
                 print('Command not recognized')
-                
+            
+            
+            
+            
+            
         elif user_input == 'Q' or user_input == 'q':
             break
         
         else:
             print('Command not recognized. ')
-            
+        
+        
+        
+
+
 
 if __name__ == '__main__':
     main()
-
-
 
 
 
